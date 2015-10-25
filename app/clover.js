8fb9af2135e6babcb7619471a9231fe7657d441a
Matthew Kuzyk <matthew.kuzyk@gmail.com>
2015-10-25 02:16:44 -0700

    New inventory items

--- app/clover.js
+++ app/clover.js
@@ -1,29 +1,20 @@
 var async  = require('async');
-var jQuery = require('jquery');
+var r = require('superagent');
 
 var MERCHANT_ID = 'FWAR5KMC2349C';
 
 var apiURL = 'https://apisandbox.dev.clover.com/v3/merchants/'+MERCHANT_ID;
 
-var call = function(endpoint, method, data, callback) {
+var call = function(endpoint, method, data, next) {
   if (endpoint.charAt(0) !== "/") {
     endpoint = "/" + endpoint;
   }
-  return jQuery.ajax({
-    type     : method,
-    url      : apiURL + endpoint,
-    dataType : "text",
-    data     : data,
-    success  : function(resp) {
-      callback(resp);
-    },
-    error    : function(resp) {
-      console.log(resp);
-    },
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('Authorization', 'Bearer a206ae20-b1fb-6ba4-1d92-8403105d2f91');
-    }
-  });
+
+  if (method === "POST") {
+    r.post(apiURL + endpoint).send(data).set('Authorization', 'Bearer a206ae20-b1fb-6ba4-1d92-8403105d2f91').end(next)
+  }
+  else
+    r.get(apiURL + endpoint).set('Authorization', 'Bearer a206ae20-b1fb-6ba4-1d92-8403105d2f91').end(next)
 };
 
 module.exports = {
--- app/routes/twitter.js
+++ app/routes/twitter.js
@@ -1,9 +1,11 @@
 var twitter = require('../twitter')
-var _    = require('underscore');
+var _    = require('lodash');
 var async = require('async')
+var clover = require('../clover')
+var moment = require('moment')
 
 module.exports = function(router) {
-  router.route('/twitter/events')
+  router.route('/social')
     .get(function(req, res) {
         async.auto({
           getNFL: function(callback) {
@@ -12,14 +14,22 @@ module.exports = function(router) {
           getCancer: function(callback) {
             twitter.scrapeEvent('Breast Cancer', callback)
           },
-          getLoL : function(callback) {
-            twitter.scrapeEvent('League of Legends', callback)
+          getMoney : function(callback) {
+            twitter.scrapeEvent('Money 20/20', callback)
           },
           getHalloween : function(callback) {
             twitter.scrapeEvent('Halloween', callback)
+          },
+          getNewInventory: function(callback) {
+            clover.call('items', 'GET', {}, function(err, result) {
+              var new_items = _.filter(result.body.elements, function(item) {
+                return item.modifiedTime > moment().hour(-1).valueOf()
+              })
+              callback(null, new_items)
+            })
           }
         }, function(err, results) {
-            res.json([results.getNFL, results.getCancer, results.getLoL, results.getHalloween])
+            res.json([results.getNFL, results.getCancer, results.getMoney, results.getHalloween, results.getNewInventory])
         })
     })
 };
